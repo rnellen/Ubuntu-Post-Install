343b7760f1b6dcb738f2ee08a141c2171e6232d7
Roberto Nellen <34263242+rnellen@users.noreply.github.com>
2021-05-26 03:27:22 +0200

    cleanup echo command

--- post-install.sh
+++ post-install.sh
@@ -30,44 +30,46 @@ mv /etc/ssh/ssh_config /etc/ssh/ssh_config-orig
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config-orig
 
 # Create new SSH config files (disable root login, keybased login, hardening)
-echo -n "" > /etc/ssh/ssh_config
-echo "Host *" >> /etc/ssh/ssh_config
-echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config
-echo "    ChallengeResponseAuthentication no" >> /etc/ssh/ssh_config
-echo "    PubkeyAuthentication yes" >> /etc/ssh/ssh_config
-echo "    SendEnv LANG LC_*" >> /etc/ssh/ssh_config
-echo "    HashKnownHosts yes" >> /etc/ssh/ssh_config
-echo "    GSSAPIAuthentication yes" >> /etc/ssh/ssh_config
-echo "    HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa" >> /etc/ssh/ssh_config
-echo "    KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256" >> /etc/ssh/ssh_config
-echo "    Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr" >> /etc/ssh/ssh_config
-echo "    MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com" >> /etc/ssh/ssh_config
+echo "
+Host *
+    PasswordAuthentication no
+    ChallengeResponseAuthentication no
+    PubkeyAuthentication yes
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
+    HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+    KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+    Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+    MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+" >> /etc/ssh/ssh_config
 
-echo -n "" > /etc/ssh/sshd_config
 echo "
 ###########################################
 Please provide a new SSH port number
 ###########################################
 "
 read sshport
-echo "Port $sshport" >> /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "UsePAM yes" >> /etc/ssh/sshd_config
-echo "X11Forwarding yes" >> /etc/ssh/sshd_config
-echo "PrintMotd no" >> /etc/ssh/sshd_config
-echo "AcceptEnv LANG LC_*" >> /etc/ssh/sshd_config
-echo "Subsystem	sftp	/usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config
-echo "KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256" >> /etc/ssh/sshd_config
-echo "Protocol 2" >> /etc/ssh/sshd_config
-echo "HostKey /etc/ssh/ssh_host_ed25519_key" >> /etc/ssh/sshd_config
-echo "HostKey /etc/ssh/ssh_host_rsa_key" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
-echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
-echo "AllowGroups ssh-user" >> /etc/ssh/sshd_config
-echo "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr" >> /etc/ssh/sshd_config
-echo "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com" >> /etc/ssh/sshd_config
-echo "AuthorizedKeysFile     %h/.ssh/authorized_keys" >> /etc/ssh/sshd_config
+echo "
+Port $sshport
+PermitRootLogin no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem	sftp	/usr/lib/openssh/sftp-server
+KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+Protocol 2
+HostKey /etc/ssh/ssh_host_ed25519_key
+HostKey /etc/ssh/ssh_host_rsa_key
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+PubkeyAuthentication yes
+AllowGroups ssh-user
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+AuthorizedKeysFile     %h/.ssh/authorized_keys
+" >> /etc/ssh/sshd_config
 
 awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
 mv "${HOME}/moduli" /etc/ssh/moduli
@@ -89,6 +91,18 @@ su - $user -c "ssh-import-id gh:rnellen"
 
 # Restart sshd
 systemctl restart sshd
+
+
+# Firewall
+
+# Fail2Ban
+
+# Motd
+
+# SpeedTest Install
+
+# Docker option install 
+
 
 
 
