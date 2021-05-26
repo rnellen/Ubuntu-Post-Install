00ad5f3a6073b549f2c44e486f62ad6d895bf66a
Roberto Nellen <34263242+rnellen@users.noreply.github.com>
2021-05-26 11:12:57 +0200

    Update post-install-copy.sh

--- post-install-copy.sh
+++ post-install-copy.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-# Goal: Script which automatically sets up a new Ubuntu Machine after installation
-# This is a basic install, easily configurable to your needs
 
-# Test to see if user is running with root privileges.
-if [[ "${UID}" -ne 0 ]]
-then
- echo 'Must execute with sudo or root' >&2
- exit 1
-fi
 
-# Ensure system is up to date
-sudo apt-get update -y 
-
-# Upgrade the system
-sudo apt-get upgrade -y
-
-# Install OpenSSH
-sudo apt-get install openssh-server -y
-
-# Enable Firewall
-sudo ufw enable 
-
-# configure the firewall
-sudo ufw allow OpenSSH
-
-# Disabling root login 
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config 
-echo "PermitEmptyPasswords no" /etc/ssh/sshd_config
 
 # Message of the day 
 sudo wget https://raw.githubusercontent.com/jwandrews99/Linux-Automation/master/misc/motd.sh
@@ -44,19 +18,7 @@ echo "Unattended-Upgrade::Allowed-Origins {
 #Unattended-Upgrade::Automatic-Reboot "true"; 
 #}; " >> /etc/apt/apt.conf.d/50unattended-upgrades
 
-# Fail2Ban install 
-sudo apt-get install -y fail2ban
-sudo systemctl start fail2ban
-sudo systemctl enable fail2ban
 
-echo "
-[sshd]
-enabled = true
-port = 22
-filter = sshd
-logpath = /var/log/auth.log
-maxretry = 4
-" >> /etc/fail2ban/jail.local
 
 # SpeedTest Install
 sudo apt-get install speedtest-cli -y
@@ -128,9 +90,7 @@ else
     exit 1
 fi
 
-# Cleanup
-sudo apt autoremove
-sudo apt clean 
+
 
 echo "
 ######################################################################################################
